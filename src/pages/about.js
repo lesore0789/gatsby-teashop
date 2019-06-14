8297e9dff6dd7c7ff1d83c8cf42ff46d84fe308f
lesore0789 <lesore0789@gmail.com>
2019-06-14 01:34:33 -0500

    Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/Info"
+
+const AboutPage = ({ data }) => (
+  <Layout>
+    <SEO title="About" />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="About us"
+      styleClass="about-background"
+    />
+    <Info />
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "tea1.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
+
+export default AboutPage
